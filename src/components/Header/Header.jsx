3fe90dd162d1a02cac7a018997a2e1ba092d5c78
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-10-08 16:28:27 +0300

    add task2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unknown-property */
 import { Link } from 'react-router-dom';
 import './Header.scss';
 import { useEffect, useState } from 'react';
@@ -63,9 +62,9 @@ export const Header = () => {
       <div className="burger-button" onClick={toggleBurgerMenu}>
         {!isBurgerOpen ? (
           <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M4 18L20 18" stroke="#ffffff" stroke-width="2" stroke-linecap="round"/>
-          <path d="M4 12L20 12" stroke="#ffffff" stroke-width="2" stroke-linecap="round"/>
-          <path d="M4 6L20 6" stroke="#ffffff" stroke-width="2" stroke-linecap="round"/>
+          <path d="M4 18L20 18" stroke="#ffffff" strokeWidth="2" strokeLinecap="round"/>
+          <path d="M4 12L20 12" stroke="#ffffff" strokeWidth="2" strokeLinecap="round"/>
+          <path d="M4 6L20 6" stroke="#ffffff" strokeWidth="2" strokeLinecap="round"/>
           </svg>
         ) : (
           <svg width="30px" height="30px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#000000" d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504 738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512 828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496 285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512 195.2 285.696a64 64 0 0 1 0-90.496z"/></svg>
--- src/components/shared/Task2/Task2.jsx
+++ src/components/shared/Task2/Task2.jsx
@@ -1,10 +1,103 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-undef */
+import { useState, useEffect } from 'react';
+import imagesData from './../../../assets/data/data.json';
 import './Task2.css';
 
 export const Task2 = () => {
+  const images = imagesData.images;
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [deletedImageIds, setDeletedImageIds] = useState(() => {
+    const storedDeletedImageIds = localStorage.getItem('deletedImageIds');
+    return storedDeletedImageIds ? JSON.parse(storedDeletedImageIds) : [];
+  });
+  const [visibleImageCount, setVisibleImageCount] = useState(images.length - deletedImageIds.length);
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  useEffect(() => {
+    localStorage.setItem('deletedImageIds', JSON.stringify(deletedImageIds));
+    updateVisibleImageCount();
+  }, [deletedImageIds]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  const updateVisibleImageCount = () => {
+    setVisibleImageCount(images.length - deletedImageIds.length);
+  };
+
+
+  const handleRestoreImages = () => {
+    setDeletedImageIds([]);
+    scrollToTop();
+  };
+
+  const handleDeleteImage = (imageId) => {
+    setDeletedImageIds((prevDeletedImageIds) => [...prevDeletedImageIds, imageId]);
+    scrollToTop();
+  };
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
   
   return (
-    <div>
-      task2
+    <div className="container">
+      <div className="page-info">
+        <p>{`Images counter: ${visibleImageCount}`}</p>
+        <p>{`Date: ${currentDate.toLocaleString()}`}</p>
+      </div>
+      <div className="image-grid">
+        {images.map((image) => (
+          !deletedImageIds.includes(image.id) && (
+            <div key={image.id} className="image-container">
+              <img
+                src={image.src}
+                alt={image.alt}
+                onClick={() => handleImageClick(image)}
+              />
+              <span
+                className="delete-button"
+                onClick={() => handleDeleteImage(image.id)}
+              >
+                х
+              </span>
+            </div>
+          )
+        ))}
+      </div>
+      {selectedImage && (
+        <div className="overlay">
+          <div className="modal">
+            <img 
+              src={selectedImage.src} 
+              alt={selectedImage.alt}
+              className="loaded"
+            />
+            <button 
+              className="close-button"
+              onClick={() => setSelectedImage(null)}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+      <button className="restore-button" onClick={handleRestoreImages}>
+        <p>Restore deleted</p>
+      </button>
     </div>
   );
-}
+};
