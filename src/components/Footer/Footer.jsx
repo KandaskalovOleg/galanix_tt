7e94dfde5cf4864f79dc001912556ce7c10e9f41
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-10-08 11:51:59 +0300

    add and stylezed footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import './Footer.css';
+import GitHubSvg from './../../assets/logo/github.svg';
 
 export const Footer = () => {
   
   return (
-    <footer>
-      footer
+    <footer className='footer'>
+      <div className='footerBlock'>
+        <a href="https://github.com/KandaskalovOleg/galanix_tt" className='footerLink'>
+          <img src={GitHubSvg} alt="gihub logo" />
+        </a>
+      </div>
     </footer>
   );
 }
