1f97c8f58d912093b4f45372272fc82bd060982e
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-10-09 13:36:07 +0300

    save country data after reload page

--- src/components/shared/Task3/Task3.jsx
+++ src/components/shared/Task3/Task3.jsx
@@ -1,26 +1,37 @@
-import { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from 'react';
 import { MagnifyingGlass } from 'react-loader-spinner';
 import './Task3.css';
 
 export const Task3 = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [country, setCountry] = useState('');
-  const [universities, setUniversities] = useState();
+  const [universities, setUniversities] = useState([]);
   const [error, setError] = useState('');
+  const [selectedCount, setSelectedCount] = useState(0);
+
+  useEffect(() => {
+    const savedCountry = localStorage.getItem('country');
+    if (savedCountry) {
+      handleSearch(savedCountry);
+    }
+  }, []);
 
   const handleInputChange = (e) => {
     setCountry(e.target.value);
   };
 
-  const handleSearch = async () => {
+  const handleSearchFromField = () => {
+    handleSearch(country);
+  };
+
+  const handleSearch = async (searchCountry) => {
     setIsLoading(true);
     handleReset();
 
-
     try {
       setError('');
-      const response = await fetch(`http://universities.hipolabs.com/search?country=${country}`);
-      
+      const response = await fetch(`http://universities.hipolabs.com/search?country=${searchCountry}`);
       if (!response.ok) {
         throw new Error('Request error.');
       }
@@ -33,21 +44,30 @@ export const Task3 = () => {
 
       setUniversities(data);
       setIsLoading(false);
-
+      localStorage.setItem('country', searchCountry);
     } catch (error) {
       setError(error.message);
-      setUniversities();
+      setUniversities([]);
       setIsLoading(false);
     }
   };
 
   const handleReset = () => {
     setCountry('');
-    setUniversities();
-    localStorage.removeItem('country');
+    setUniversities([]);
     setError('');
+    setSelectedCount(0);
+    localStorage.removeItem('country');
   };
-  
+
+  const handleCheckboxChange = (index) => {
+    const updatedUniversities = [...universities];
+    updatedUniversities[index].isSelected = !updatedUniversities[index].isSelected;
+    setUniversities(updatedUniversities);
+
+    const selectedCount = updatedUniversities.filter((university) => university.isSelected).length;
+    setSelectedCount(selectedCount);
+  };
 
   return (
     isLoading ? (
@@ -57,13 +77,13 @@ export const Task3 = () => {
           width="80"
           ariaLabel="MagnifyingGlass-loading"
           wrapperClass="MagnifyingGlass-wrapper"
-          glassColor = '#eee'
-          color = '#333'
+          glassColor="#eee"
+          color="#333"
         />
       </div>
     ) : (
-      <div className='content-wrapper'>
-        <div className='search-block'>
+      <div className="content-wrapper">
+        <div className="search-block">
           <input
             type="text"
             className="input"
@@ -71,39 +91,50 @@ export const Task3 = () => {
             value={country}
             onChange={handleInputChange}
           />
-          <button className="button" onClick={handleSearch}>Send</button>
-          <button className='button' onClick={handleReset}>Reset</button>
+          <button className="button" onClick={handleSearchFromField}>Send</button>
+          <button className="button" onClick={handleReset}>Reset</button>
         </div>
         <p className="error">{error}</p>
-        { universities && (
-          <div className="scrolling-container" style={{ overflowX: 'auto', maxWidth: '100%' }}>
-          <table className="table">
-            <thead className="thead">
-              <tr>
-                <th className="th">#</th>
-                <th className="th">Назва університету</th>
-                <th className="th">Країна</th>
-                <th className="th">Сайт</th>
-              </tr>
-            </thead>
-            <tbody className="tbody">
-              {universities.map((university, index) => (
-                <tr key={index + 1} className='tr'>
-                  <td className="td">{index + 1}</td>
-                  <td className="td">{university.name}</td>
-                  <td className="td">{university.country}</td>
-                  <td className="td">
-                    {university.web_pages.map((webPage) => (
-                      <a key={webPage} href={webPage} target="_blank" rel="noopener noreferrer">
-                        {webPage}
-                      </a>
-                    ))}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          </div>
+        {universities.length > 0 && (
+          <>
+            <p>Cheacboxes counter: {selectedCount}</p>
+            <div className="scrolling-container" style={{ overflowX: 'auto', maxWidth: '100%' }}>
+              <table className="table">
+                <thead className="thead">
+                  <tr>
+                    <th className="th">#</th>
+                    <th className="th">Universities</th>
+                    <th className="th">Country</th>
+                    <th className="th">Site</th>
+                    <th className="th">Add to my list</th>
+                  </tr>
+                </thead>
+                <tbody className="tbody">
+                  {universities.map((university, index) => (
+                    <tr key={index + 1} className="tr">
+                      <td className="td">{index + 1}</td>
+                      <td className="td">{university.name}</td>
+                      <td className="td">{university.country}</td>
+                      <td className="td">
+                        {university.web_pages.map((webPage) => (
+                          <a key={webPage} href={webPage} target="_blank" rel="noopener noreferrer">
+                            {webPage}
+                          </a>
+                        ))}
+                      </td>
+                      <td className="td">
+                        <input
+                          type="checkbox"
+                          checked={university.isSelected}
+                          onChange={() => handleCheckboxChange(index)}
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </>
         )}
       </div>
     )
